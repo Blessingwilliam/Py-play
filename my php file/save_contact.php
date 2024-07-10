7c7b7e3595820e148b77f0fef42ce24fb71f911c
Blessingwilliam <138787249+Blessingwilliam@users.noreply.github.com>
2024-07-10 17:28:12 +0300

    Delete my php file directory

--- my php file/save_contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $phone = $_POST['phone'];
-    $email = $_POST['email'];
-    $address = $_POST['address'];
-    $reg_number = $_POST['reg_number'];
-    echo "Contact details saved successfully.";
-}
-?>
